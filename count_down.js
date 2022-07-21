ca34a488655c87be376b69746cbaff9c32131a3e
dotnotation <dotinmotionproductions@gmail.com>
2022-07-21 11:24:16 -0400

    added notes about function

--- count_down.js
+++ count_down.js
@@ -1,10 +1,16 @@
 function countDown(num){
+    // on the first pass, the first if statement is skipped because num = 5
     if(num <= 0){
         console.log("All done!")
+        // if we didn't return, the code below would run when we didn't want it to
+        // thus we need a return to end the function
         return
     }
+    // we console log 5
     console.log(num)
+    // then subtract 1 from 5 and now num = 4
     num--
+    // we then call the function from within our function and pass in the new num which is 4 and run through everything again
     countDown(num)
 }
 
