7a69a5ca848d62e8c04e02ef1e92405523932ba9
dotnotation <dotinmotionproductions@gmail.com>
2022-07-21 11:17:47 -0400

    added solution without recursion

--- count_down.js
+++ count_down.js
@@ -10,3 +10,11 @@ function countDown(num){
 
 countDown(5)
 // this will print out 5, 4, 3, 2, 1, All done!
+
+// without recursion
+function countDown(num){
+    for (var i = num; i > 0; i--){
+        console.log(i)
+    }
+    console.log("All done!")
+}
