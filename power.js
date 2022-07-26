7f6834453630daeb156ac8ec7667d6c42dbec240
dotnotation <dotinmotionproductions@gmail.com>
2022-07-26 18:23:34 -0400

    started power function and notes

---
+++ power.js
@@ -0,0 +1,11 @@
+// takes in a base and an exponent
+// return the power of the base to the exponent
+// mimic the functionality of Math.pow()
+// no negatives
+// ex. power(2, 0) = 1
+// power(2, 2) = 4
+// power(2, 4) = 16
+
+function power(base, exp){
+
+}
