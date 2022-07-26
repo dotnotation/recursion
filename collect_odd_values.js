1b5e7b010c180c39548947d7fb3e35c12476b7bf
dotnotation <dotinmotionproductions@gmail.com>
2022-07-26 18:17:15 -0400

    added pure recursive answer to collectOddValues and notes

--- collect_odd_values.js
+++ collect_odd_values.js
@@ -1,3 +1,5 @@
+// Helper Method
+
 function collectOddValues(arr){
     let result = []
 
@@ -22,4 +24,37 @@ function collectOddValues(arr){
     helper(arr)
 
     return result
+}
+
+// Pure Recursion 
+
+function collectOddValues(arr){
+    let newArr = []
+    // this will technically be reset on each recursive call
+
+    if (arr.length === 0){
+        return newArr
+    }
+
+    if (arr[0] % 2 !== 0){
+        newArr.push(arr[0])
+    }
+
+    newArr = newArr.concat(collectOddValues(arr.slice(1)))
+    // say our array was [1, 2, 3, 4, 5]
+    // basically this is the current array value .concat and then calling the collectOddValues recursively on the rest of the array
+    // [1].concat(collectOddValues([2, 3, 4, 5]))
+    // we are slicing off the first element of the array which is 1
+    // then we are calling our function on the rest of the array [2, 3, 4, 5]
+    // then the array goes back through the function
+    // the let newArr = [] will be empty on this next call
+    // then we finally get back to line 43
+    // [].concat(collectOddValues([3, 4, 5]))
+    // the newArr will be empty this time because 2 is an even number so it isn't being pushed into newArr
+    // the next call will be [3].concat(collectOddValues([4, 5]))
+    // once you get to 5 and have an empty arr, then you will hit line 35
+    // once you return an empty array, then the top of the call stack has been hit
+    // [5].concat(collectOddValues([5])) will return 5, the even values will return nothing
+    // after the call stack is complete, you will have an array of the odd numbers
+    return newArr
 }
