9b388f4472953ba174cac34671308dad2f6b4415
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-09-05 15:35:18 -0400

    added solution and notes to power function

--- power.js
+++ power.js
@@ -5,7 +5,11 @@
 // ex. power(2, 0) = 1
 // power(2, 2) = 4
 // power(2, 4) = 16
+// recursion so we want to call our function within the function
 
 function power(base, exp){
-
+    if(exp === 0) return 1
+    return base * power(base, exp-1)
+    // so we want to take the base and times it by the base number and repeat it exp amount of times
+    // so the base will be times by itself for the amount of exp there is 
 }
