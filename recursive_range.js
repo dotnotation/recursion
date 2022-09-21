a94ee0599f5e65aab80e30d097664402d329e0dd
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-09-21 18:12:32 -0400

    added notes and psuedocode for recursiveRange function

---
+++ recursive_range.js
@@ -0,0 +1,9 @@
+// accepts a numbers
+// adds all numbers from 0 to the number passed
+// ex. recursiveRange(6) = 21
+// recursiveRange(10) = 55
+
+function recursiveRange(num){
+    // if num equals 0 return 
+    // call recursiveRange add num plus num -1
+}
