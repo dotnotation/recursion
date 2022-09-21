27c4ddc66da2288424966a917e574a3799f95332
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-09-21 16:41:15 -0400

    added solution and notes to productOfArray

--- product_of_array.js
+++ product_of_array.js
@@ -4,6 +4,10 @@
 // productOfArray([1, 2, 3, 10]) = 60
 
 function productOfArray(arr){
-    //call the function to add each number together
-
+    //call the function to multiply each number together
+    if (arr.length === 0) return 1
+    // need to account for array being empty
+    // call function recursively
+    // slice array
+    return arr[0] * productOfArray(arr.slice(1))
 }
