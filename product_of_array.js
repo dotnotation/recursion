fd42ee37067a8a998935764b31ac7b2f45f95cee
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-09-21 16:33:33 -0400

    added examples

--- product_of_array.js
+++ product_of_array.js
@@ -1,5 +1,8 @@
 // takes in an array of numbers
 // returns the product of them all
+// ex. productOfArray([1, 2, 3]) = 6
+// productOfArray([1, 2, 3, 10]) = 60
+
 function productOfArray(arr){
     //call the function to add each number together
 
