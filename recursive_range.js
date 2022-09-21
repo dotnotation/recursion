3ab0fa6b6f15005751ee128a9031f6f8088f2195
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-09-21 18:13:50 -0400

    added solution to recursiveRange

--- recursive_range.js
+++ recursive_range.js
@@ -5,5 +5,7 @@
 
 function recursiveRange(num){
     // if num equals 0 return 
+    if (num === 0) return num
     // call recursiveRange add num plus num -1
+    return num + recursiveRange(num - 1)
 }
