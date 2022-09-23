e8f81a9c6d25e026c9cae3a6ec0c9719d55b1ada
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-09-23 10:49:46 -0400

    added base function and notes for fib function

---
+++ fib.js
@@ -0,0 +1,10 @@
+// accepts a number
+// returns that number in the Fibonacci sequence
+// The Fibonacci sequence starts at 1 
+// every number thereafter is equal to the sum of the previous two numbers
+// ex. fib(4) = 3
+// fib(10) = 55
+
+function fib(){
+
+}
