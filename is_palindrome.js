559026230393b4984ccffbebeebee7735bd1b9e2
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-09-26 13:29:34 -0400

    added notes and finished solution

--- is_palindrome.js
+++ is_palindrome.js
@@ -3,7 +3,11 @@
 
 function isPalindrome(str){
     // true: str.length <= 1, str.length === 2 and str[0] === str[1], if palindrome
+    // compare the first and last characters
+    // then slice off those characters
+    // repeat until one of the other ifs is met
     if (str.length <= 1) return true
     if (str.length === 2 && str[0] === str[1]) return true
-    return isPalindrome(str)
+    if (str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1))
+    return false
 }
