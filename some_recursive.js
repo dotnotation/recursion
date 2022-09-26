5ed72d25462d755a019cfe54132a5e43ce0bd94b
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-09-26 14:01:35 -0400

    added base notes and function

---
+++ some_recursive.js
@@ -0,0 +1,9 @@
+// accepts an array and a callback
+// returns true if a single value in the array returns true when passed to the callback
+// returns false otherwise
+// callback: isOdd = val => val % 2 !== 0
+// ex. someRecursive([1, 2, 3, 4], isOdd) = true
+
+function someRecursive(arr, call){
+    
+}
