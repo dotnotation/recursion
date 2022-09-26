40b9e823475efa94380cfdf0acfda36193f19535
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-09-26 13:05:08 -0400

    added notes

--- reverse.js
+++ reverse.js
@@ -4,4 +4,13 @@
 function reverse(str){
     if (str === "") return ""
     return reverse(str.substr(1)) + str.charAt(0)
+    // substr returns a portion of the string starting at the specified index and extending for a given number of characters afterwards
+    // ex. reverse("awesome") str.substr(1) = "wesome" 
+    // charAt returns a new string consisting of the single letter located at the specified index
+    // ex. reverse("awesome") str.charAt(0) = "a"
+    // putting it together
+        // reverse("awesome")
+        // str.substr(1) = "wesome"
+        // str.charAt(0) = "a"
+        // "wesomea"
 }
