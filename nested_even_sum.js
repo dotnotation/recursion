3a9b4d3d2ed0282dfb62253147b13b3c6da3a1ea
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-11-16 09:42:28 -0500

    added notes and started function

--- nested_even_sum.js
+++ nested_even_sum.js
@@ -1,8 +1,17 @@
 // return the sum of all EVEN numbers in an OBJECT
 // The object may contain nested objects
 
-function nestedEvenSum(obj){
-
+function nestedEvenSum(obj, sum=0){
+  // access via keys
+  // for each key check if it is a number
+  // if it is a number, check to see if it is even or divisible by 2
+  // if it is even, add it to the sum
+  // if the key is not a number, recursively call the function to go to the next key?
+  // need a sum variable to return
+  for (var key in obj){
+    
+  }
+  return sum
 }
 
 var obj1 = {
