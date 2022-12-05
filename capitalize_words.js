2a0033e27f2e2227f92070a58266d2c78b6e573e
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-05 14:53:38 -0500

    finished solution and notes on solution

--- capitalize_words.js
+++ capitalize_words.js
@@ -6,6 +6,19 @@
 // ex. let words = ["i", "am", "learning", "recursion"]
 // returns ["I", "AM", "LEARNING", "RECURSION"]
 
-function capitalizeWords(){
-
+function capitalizeWords(arr){
+    // iterate through arr
+    // if it is just one word, can just upper case
+    if (arr.length === 1){
+        return [arr[0].toUpperCase()]
+    } 
+    
+    let result = capitalizeWords(arr.slice(0, -1))
+    // this will return the array minus the last element
+    // this will be run until the array is just the first element which will then run line 12-13
+    // then it will go to line 20
+    result.push(arr.slice(arr.length - 1)[0].toUpperCase())
+    // push adds to the end of the array
+    // this will go through the remaining elements of the array and capitalize it
+    return result
 }
