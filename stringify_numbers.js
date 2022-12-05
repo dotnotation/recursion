595384935b3247483f4f4cd68afd5578bc558574
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-05 15:15:43 -0500

    adding notes and sample object

--- stringify_numbers.js
+++ stringify_numbers.js
@@ -1,3 +1,20 @@
-function stringifyNumbers(){
+// takes in an object
+// find all values that are numbers
+// convert those numbers into strings
+
+function stringifyNumbers(obj){
+    // iterate through each key value pair
     
+}
+
+let obj = {
+    num: 1,
+    test: [],
+    data: {
+        val: 4,
+        info: {
+            isRight: true,
+            random: 66
+        }
+    }
 }
