bfea285bce7ab999f2f6e610f7d8de3275b2bc7d
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-05 14:07:07 -0500

    adding notes on prompt

--- capitalize_words.js
+++ capitalize_words.js
@@ -1,3 +1,11 @@
+// Given an array of words
+// Return a new array containing each word capitalized
+// By capitalized, every letter in every word is capital
+// all letters in the even array are lower case to begin with
+// no special characters or spaces
+// ex. let words = ["i", "am", "learning", "recursion"]
+// returns ["I", "AM", "LEARNING", "RECURSION"]
+
 function capitalizeWords(){
-    
+
 }
