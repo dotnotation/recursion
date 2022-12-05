399010be2a2cd419cfdfbfb7f256bb1f0ed5d30e
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-05 15:38:57 -0500

    added notes and started coding solution

--- collect_strings.js
+++ collect_strings.js
@@ -1,3 +1,32 @@
-function collectStrings(){
-    
+// takes in an object
+// returns an array of all the values that are a string
+
+function collectStrings(obj){
+
+    let results = []
+
+    for (var key in obj){
+        if (typeof obj[key] === 'string'){
+            results.push(obj[key])
+        }
+    }
+
 }
+
+const obj = {
+    stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz"
+                    }
+                }
+            }
+        }
+    }
+}
+
+collectStrings(obj) // ["foo", "bar", "baz"])
